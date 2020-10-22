c73d76234abe4a0248003711883b59b7fad896d0
ernest annor <we2annor@gmail.com>
2020-10-22 08:01:58 +0100

    changed case in error component

--- server/client/src/components/error/Error.jsx
+++ server/client/src/components/error/Error.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../../sass/components/error.scss";
 
 const Error = () => {
-  return <div className='error'>Sorry An Error Occured</div>;
+  return <div className='error'>Sorry an error cccured</div>;
 };
 
 export default Error;
